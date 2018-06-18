476db19c9550df6465ae40150eafc55949d621d8
cristiano-hahn <cristianohahn.ch@gmail.com>
2018-06-18 19:20:07 -0300

    Hello world!

---
+++ src/main/java/com/vendaComputadores/vendaComp/VendaCompApplication.java
@@ -0,0 +1,12 @@
+package com.vendaComputadores.vendaComp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaCompApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaCompApplication.class, args);
+	}
+}
