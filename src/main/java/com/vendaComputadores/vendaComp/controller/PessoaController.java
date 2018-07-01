942dad901fd964c71bf7d06a9ff5b26df5f8ce2a
mauriciozatt@hotmail.com <mauriciozatt@hotmail.com>
2018-07-01 20:59:04 -0300

    alterações usuario e cadastro clientes

---
+++ src/main/java/com/vendaComputadores/vendaComp/controller/PessoaController.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vendaComputadores.vendaComp.controller;
+
+/*
+ @author mauricio
+ */
+import com.vendaComputadores.vendaComp.model.Pessoa;
+import com.vendaComputadores.vendaComp.service.PessoaService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import static sun.misc.ClassFileTransformer.add;
+
+@Controller()
+public class PessoaController {
+
+    @Autowired
+    private PessoaService service;
+
+    @GetMapping("/pessoa")
+    public ModelAndView findAll() {
+        //O método vai exibir a página produto.html
+        ModelAndView mv = new ModelAndView("/pessoa");
+
+        mv.addObject("pessoas", service.findAll());
+
+        return mv;
+    }
+
+    @PostMapping("pessoa/save")
+    public ModelAndView save(@Valid Pessoa pessoa, BindingResult result) {
+        if (result.hasErrors()) {
+            return add(pessoa);
+        }
+
+        service.save(pessoa);
+
+        return findAll();
+    }
+
+    @GetMapping("pessoa/add")
+    private ModelAndView add(Pessoa pessoa) {
+        ModelAndView mv = new ModelAndView("/pessoaAdd");
+        mv.addObject("pessoa", pessoa);
+        return mv;
+    }
+
+    @GetMapping("pessoa/edit/{id}")
+    public ModelAndView edit(@PathVariable("id") Long id) {
+        return add(service.findOne(id));
+    }
+
+    @GetMapping("pessoa/delete/{id}")
+    public ModelAndView delete(@PathVariable("id") Long id) {
+        service.delete(id);
+        return findAll();
+    }
+
+}
